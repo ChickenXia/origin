794c45dfe5afbf571b18ada12471f323a13cbf92
shawn_pro <1789122912@qq.com>
2020-03-10 22:46:14 +0800

    mybatis

---
+++ mybatis/IPersistence_test/src/main/java/com/lagou/test/IPersistenceTest.java
@@ -0,0 +1,53 @@
+package com.lagou.test;
+
+import com.lagou.dao.IUserDao;
+import com.lagou.io.Resources;
+import com.lagou.pojo.User;
+import com.lagou.sqlSession.SqlSession;
+import com.lagou.sqlSession.SqlSessionFactory;
+import com.lagou.sqlSession.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.InputStream;
+
+public class IPersistenceTest {
+
+    @Test
+    public void test() throws Exception {
+        InputStream resourceAsSteam = Resources.getResourceAsSteam("sqlMapConfig.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsSteam);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        //调用
+        User user = new User();
+        user.setId(6);
+        user.setUsername("lisa");
+      /*  User user2 = sqlSession.selectOne("user.selectOne", user);
+
+        System.out.println(user2);*/
+
+       /* List<User> users = sqlSession.selectList("user.selectList");
+        for (User user1 : users) {
+            System.out.println(user1);
+        }*/
+
+        IUserDao userDao = sqlSession.getMapper(IUserDao.class);
+        //insert data
+        userDao.saveUser(user);
+
+        //update data
+//        userDao.updateUser(user);
+
+        //delete data
+//        userDao.deleteUser(user);
+//        List<User> all = userDao.findAll();
+//        for (User user1 : all) {
+//            System.out.println(user1);
+//        }
+
+
+    }
+
+
+
+}
